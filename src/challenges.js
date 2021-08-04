cabe1e3798f723a243746e3e4e95d2113e79e50c
Italo Sergio <italosergio@mail.com>
2021-08-04 04:16:51 -0300

    Resolve Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -82,21 +82,22 @@ function catAndMouse(mouse, cat1, cat2) {
 
 // Desafio 8
 function fizzBuzz(array) {
-
-   for (i in array){
-    
-    if (array[i]%5 === 0 && array[i]%3 === 0){
-        novoArray.push('fizzBuzz');
-    }else if(array[i]%5 === 0){
-        novoArray.push('buzz');
-    }else if (array[i]%3 === 0){
-        novoArray.push('fizz');
-    }else{
-        novoArray.push("bug!");
+  let novoArray = []
+     for (i in array){
+      
+      if (array[i]%5 === 0 && array[i]%3 === 0){
+          novoArray.push('fizzBuzz');
+      }else if(array[i]%5 === 0){
+          novoArray.push('buzz');
+      }else if (array[i]%3 === 0){
+          novoArray.push('fizz');
+      }else{
+          novoArray.push("bug!");
+      }
+      
     }
-return novoArray
-
-}
+    return novoArray
+  }
 
 // Desafio 9
 function encode() {
