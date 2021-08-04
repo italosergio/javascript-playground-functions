b9ca1f9fa7ec596d2cb60b301381b334bd65da4f
Italo Sergio <italosergio@mail.com>
2021-08-04 03:20:22 -0300

    Inicia Desafio 7 - atende os primeiros 2/3 requisitos, os primeiros

--- src/challenges.js
+++ src/challenges.js
@@ -65,8 +65,19 @@ return cont
 
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if(cat1-mouse < cat2-mouse){
+    
+    return 'cat1'
+
+  }else if (cat1-mouse > cat2-mouse){
+
+    return 'cat2'
+
+  }else if(cat1-mouse === cat2-mouse){
+
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
