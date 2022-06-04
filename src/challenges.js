6149a5a4d2c5bd7713ce36f084479a5f3fd59c9e
Italo Sergio <italosergio@graduate.org>
2022-06-04 01:06:59 -0300

    fix: lint insues

--- src/challenges.js
+++ src/challenges.js
@@ -23,8 +23,8 @@ function splitSentence(string) {
   let concatStrings = '';
   let arrayOfWords = [];
 
-  for(let letter of string) {
-    if(letter !== ' ') {
+  for (let letter of string) {
+    if (letter !== ' ') {
       concatStrings += letter;
     } else {
       arrayOfWords.push(concatStrings);
@@ -44,11 +44,11 @@ function concatName(array) {
   const firstIndex = 0;
   let newSentence = '';
 
-  for(let wordIndex = lastIndex; wordIndex >= 0; wordIndex-=1) {
-    if(wordIndex === lastIndex) {
+  for (let wordIndex = lastIndex; wordIndex >= 0; wordIndex -= 1) {
+    if (wordIndex === lastIndex) {
       newSentence = `${sentenceArray[wordIndex]}, `;
-    } else if(wordIndex === firstIndex) {
-      newSentence += sentenceArray[wordIndex]
+    } else if (wordIndex === firstIndex) {
+      newSentence += sentenceArray[wordIndex];
     }
   }
 
@@ -61,28 +61,28 @@ function footballPoints(wins, draws) {
   const victoryPoints = wins * 3;
   const drawPoints = draws * 1;
   const totalPoints = victoryPoints + drawPoints;
-  
+
   return totalPoints;
 }
 
 // Desafio 6
 
-function highestCount(array) {
-
-  function highestNumCalc(numberArray) {
-    let highestNumber = numberArray[0];
-    for(let currentValue of numberArray) {
-      if(currentValue > highestNumber) {
-        highestNumber = currentValue;
-      }
+function highestNumCalc(numberArray) {
+  let highestNumber = numberArray[0];
+  for (let currentValue of numberArray) {
+    if (currentValue > highestNumber) {
+      highestNumber = currentValue;
     }
-    return highestNumber;
   }
+  return highestNumber;
+}
 
+function highestCount(array) {
   let highestNumberCounter = 0;
   const highestNumber = highestNumCalc(array);
-  for(let currentValue of array) {
-    if(highestNumber === currentValue) {
+
+  for (let currentValue of array) {
+    if (highestNumber === currentValue) {
       highestNumberCounter += 1;
     }
   }
@@ -95,34 +95,34 @@ function catAndMouse(mouse, cat1, cat2) {
   const cat1ToMouseDistance = Math.abs(cat1 - mouse);
   const cat2ToMouseDistance = Math.abs(cat2 - mouse);
 
-  if(cat1ToMouseDistance < cat2ToMouseDistance) return 'cat1';
-  if(cat1ToMouseDistance > cat2ToMouseDistance) return 'cat2';
-  
-  return "os gatos trombam e o rato foge";
+  if (cat1ToMouseDistance < cat2ToMouseDistance) return 'cat1';
+  if (cat1ToMouseDistance > cat2ToMouseDistance) return 'cat2';
+
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
 
-function fizzBuzz(array) {
-  function defineFizzBuzzOrBug(number) {
-    const inputNumber = number;
-  
-    const isDivisibleByFive = !(inputNumber % 5);
-    const isDivisibleByThree = !(inputNumber % 3);
-  
-    if(isDivisibleByFive && isDivisibleByThree) {
-      return 'fizzBuzz';
-    } else if(isDivisibleByFive) {
-      return 'buzz';
-    } else if(isDivisibleByThree) {
-      return 'fizz';
-    }
-    return 'bug!';
-  }
+function defineFizzBuzzOrBug(number) {
+  const inputNumber = number;
 
+  const isDivisibleByFive = !(inputNumber % 5);
+  const isDivisibleByThree = !(inputNumber % 3);
+
+  if (isDivisibleByFive && isDivisibleByThree) {
+    return 'fizzBuzz';
+  } else if (isDivisibleByFive) {
+    return 'buzz';
+  } else if (isDivisibleByThree) {
+    return 'fizz';
+  }
+  return 'bug!';
+}
+
+function fizzBuzz(array) {
   let fizzBuzzBugArray = [];
 
-  for(let value of array) {
+  for (let value of array) {
     fizzBuzzBugArray.push(defineFizzBuzzOrBug(value));
   }
   return fizzBuzzBugArray;
@@ -135,24 +135,24 @@ function encode(string) {
 
   for (let value of string) {
     switch (value) {
-      case 'a':
-        encodedString += '1';
-        break;
-      case 'e':
-        encodedString += '2';
-        break;
-      case 'i':
-        encodedString += '3';
-        break;
-      case 'o':
-        encodedString += '4';
-        break;
-      case 'u':
-        encodedString += '5';
-        break;
-      default:
-        encodedString += value;
-        break;
+    case 'a':
+      encodedString += '1';
+      break;
+    case 'e':
+      encodedString += '2';
+      break;
+    case 'i':
+      encodedString += '3';
+      break;
+    case 'o':
+      encodedString += '4';
+      break;
+    case 'u':
+      encodedString += '5';
+      break;
+    default:
+      encodedString += value;
+      break;
     }
   }
   return encodedString;
@@ -163,24 +163,24 @@ function decode(string) {
 
   for (let value of string) {
     switch (value) {
-      case '1':
-        decodedString += 'a';
-        break;
-      case '2':
-        decodedString += 'e';
-        break;
-      case '3':
-        decodedString += 'i';
-        break;
-      case '4':
-        decodedString += 'o';
-        break;
-      case '5':
-        decodedString += 'u';
-        break;
-      default:
-        decodedString += value;
-        break;
+    case '1':
+      decodedString += 'a';
+      break;
+    case '2':
+      decodedString += 'e';
+      break;
+    case '3':
+      decodedString += 'i';
+      break;
+    case '4':
+      decodedString += 'o';
+      break;
+    case '5':
+      decodedString += 'u';
+      break;
+    default:
+      decodedString += value;
+      break;
     }
   }
 
